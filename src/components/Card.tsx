b1638120b0e5eccbfd0eae22237beeb688a96d54
Kubatbekkk <kubineccastro@gmail.com>
2023-03-09 23:26:12 +0600

    feat: add card__footer new element <card__footer-left>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,8 +12,33 @@ export class Card extends Component {
               alt="strawberry jam"
               className="card__img"
             />
+            <p className="card__desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quidem
+              asperioresdicta ex s dignissimos?
+            </p>
           </section>
-          <footer className="card__footer">Card Footer</footer>
+          <footer className="card__footer">
+            <div className="card__footer-left">
+              <img
+                src="https://avatars.mds.yandex.net/i?id=d597b730b96172640031b92cce4c1e2b4097398a-8502968-images-thumbs&n=13"
+                alt="strawberry jam"
+                className="card__footer-left-img"
+              />
+              <img
+                src="https://avatars.mds.yandex.net/i?id=d597b730b96172640031b92cce4c1e2b4097398a-8502968-images-thumbs&n=13"
+                alt="strawberry jam"
+                className="card__footer-left-img"
+              />
+              <img
+                src="https://avatars.mds.yandex.net/i?id=d597b730b96172640031b92cce4c1e2b4097398a-8502968-images-thumbs&n=13"
+                alt="strawberry jam"
+                className="card__footer-left-img"
+              />
+            </div>
+            <a href="#" className="card__btn">
+              more info...
+            </a>
+          </footer>
         </div>
       </div>
     );
