3bc069cd2398ca04c95e6ac0d94dc681a120c4bd
Kubatbekkk <kubineccastro@gmail.com>
2023-03-09 16:30:33 +0600

    upd: husky/pre-commit & lint-staged work

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-let a;
+console.log('check husky and lint-staged');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
