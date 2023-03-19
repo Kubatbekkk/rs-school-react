131c7231545ca2abb8d2fbddf1f292aec70a9719
Kubatbekkk <kubineccastro@gmail.com>
2023-03-20 00:08:44 +0600

    fix: add Search to Main

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import data from '../data/items';
-import Search from './Search';
 import type { CardListStateType } from 'types/types';
 
 export class CardsList extends Component<{}, CardListStateType> {
@@ -14,7 +13,6 @@ export class CardsList extends Component<{}, CardListStateType> {
   render(): JSX.Element {
     return (
       <>
-        <Search />
         <div className="main__card" data-testid="card-list">
           {this.state.items.map((item) => (
             <Card key={item.id} name={item.name} img={item.img} desc={item.desc} />
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import Container from '../components/Utils/Container';
 import React, { Component } from 'react';
 import CardsList from '../components/CardsList';
+import Search from '../components/Search';
 
 class Main extends Component {
   render(): JSX.Element {
     return (
       <Container>
         <main className="main">
+          <Search />
           <CardsList />
         </main>
       </Container>
